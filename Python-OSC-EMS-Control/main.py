5f84e3b1d94a992cda2a1813728b70793ed2f9ba
Prohurtz <48768484+RedHawk989@users.noreply.github.com>
2024-01-26 17:43:59 -0500

    clean up python code

--- Python-OSC-EMS-Control/main.py
+++ Python-OSC-EMS-Control/main.py
@@ -1,23 +1,22 @@
-# This is a sample Python script.
 from pythonosc.dispatcher import Dispatcher
 from pythonosc import udp_client
 from pythonosc import osc_server
-from pythonosc import dispatcher
 import ast
+import serial
+
+IP = "127.0.0.1"
+PORT = 2691
 
 def print_handler(address, *args):
     print(f"{address}: {args}")
 
-
 def default_handler(address, *args):
     print(f"DEFAULT {address}: {args}")
-
 
 def surge(address, *args):
     print(args)
     try:
         input_tuple = ast.literal_eval(str(args))
-
         # Additional processing to handle different types
         processed_values = []
         for value in input_tuple:
@@ -30,40 +29,27 @@ def surge(address, *args):
         # Unpacking the processed values
         amplitude, freq, time = processed_values
 
-        # Printing the values of the variables
+        # Printing the values of the variables (DEBUG)
         print("var1:", amplitude)
         print("var2:", freq)
         print("var3:", time)
+        amplitude_command = f"AMPL 1 {amplitude}\r\n".encode()
+        freq_command = f"FREQ 1 {freq}\r\n".encode()
+        time_command = f'STIM 1 {time} 1\r\n'.encode()
+        com.write(amplitude_command)
+        com.write(freq_command)
+        com.write(time_command)
 
     except (SyntaxError, ValueError):
-        pass
-    amplitude_command = f"AMPL 1 {amplitude}\r\n".encode()
-    freq_command = f"FREQ 1 {freq}\r\n".encode()
-    time_command = f'STIM 1 {time} 1\r\n'.encode()
-    pump.write(amplitude_command)
-    pump.write(freq_command)
-    pump.write(time_command)
+        print('[ERROR] BAD DATA OR COMMUNICATION FAIL.')
 
 
 dispatcher = Dispatcher()
 dispatcher.map("/EMS_TRIGGER/*", surge)
 dispatcher.set_default_handler(surge)
-
-ip = "127.0.0.1"
-port = 2691
-
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-import serial
-pump = serial.Serial(port='/dev/cu.SLAB_USBtoUART', baudrate=115200)
+com = serial.Serial(port='/dev/cu.SLAB_USBtoUART', baudrate=115200)
 
 
 if __name__ == '__main__':
-    server = osc_server.OSCUDPServer((ip, port), dispatcher)
-    server.serve_forever()  # Blocks forever
-    print('eee')
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    server = osc_server.OSCUDPServer((IP, PORT), dispatcher)
+    server.serve_forever()
