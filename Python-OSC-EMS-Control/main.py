6825691cbe4c222b4ad2decc076e5a50fbe9735b
Prohurtz <48768484+RedHawk989@users.noreply.github.com>
2024-01-26 18:33:15 -0500

    safety fault check for illegal values, more calibration fixes

--- Python-OSC-EMS-Control/main.py
+++ Python-OSC-EMS-Control/main.py
@@ -4,13 +4,14 @@ from pythonosc import osc_server
 import ast
 import serial
 
-IP = "127.0.0.1"
-PORT = 2691
+IP = "127.0.0.1" #OSC IP of application
+PORT = 2691 #OSC Port
+COM_PORT = "/dev/cu.SLAB_USBtoUART" #COM port of EMS ESP32
 
-freq = 20
-amplitude = 1
-time = 1
-calib_step = 0
+freq = 90 #STIM Frequency (in Hz)
+amplitude = 1 #STIM amplitude in mA (1-25mA)
+time = 1 #STIM on time in sec
+calib_step = 0 #Calibration step for personal tollerance calibration
 
 def print_handler(address, *args):
     print(f"{address}: {args}")
@@ -31,20 +32,34 @@ def calib(address, *args):
                 processed_values.append(str(value))
 
         # Unpacking the processed values
-        calib_step = processed_values
-        if calib_step != '0':
+        calib_step = int(processed_values)
+        
+        if calib_step != 0:
             amplitude = int(calib_step) * 2.5
         else:
             amplitude = 1
+        calib_step += 1
 
-        # Printing the values of the variables (DEBUG)
+        if calib_step == 10:
+            calib_step = 0
+
+        amplitude = float(amplitude)
+        if amplitude > 25 or amplitude < 1:
+            amplitude = 1
+            print('[WARN] AMPL VALUE OUT OF BOUNDS.')
+
+
         print("Calib_Step: ", calib_step)
 
         amplitude_command = f"AMPL 1 {amplitude}\r\n".encode()
+        freq_command = f"FREQ 1 {freq}\r\n".encode()
         time_command = f'STIM 1 {time} 1\r\n'.encode()
         com.write(amplitude_command)
+        com.write(freq_command)
+        com.write(time_command)
+
     except:
-        print('[ERROR]')
+        print('[ERROR] BAD DATA OR COMMUNICATION FAIL.')
 
 
 def surge(address, *args):
@@ -61,11 +76,26 @@ def surge(address, *args):
 
         # Unpacking the processed values
         amplitude, freq, time = processed_values
+        amplitude = float(amplitude)
+        freq = float(freq)
+        time = float(time)
+
+        if amplitude > 25 or amplitude < 1:
+            amplitude = 1
+            print('[WARN] AMPL VALUE OUT OF BOUNDS.')
+
+        if freq > 100 or freq < 1:
+            freq = 100
+            print('[WARN] FREQ OUT OF BOUNDS.')
+
+        if time > 7 or time < 0.25:
+            time = 1
+            print('[WARN] TIME OUT OF BOUNDS.')
 
         # Printing the values of the variables (DEBUG)
-        print("var1:", amplitude)
-        print("var2:", freq)
-        print("var3:", time)
+        print("AMPL:", amplitude)
+        print("FREQ:", freq)
+        print("TIME:", time)
         amplitude_command = f"AMPL 1 {amplitude}\r\n".encode()
         freq_command = f"FREQ 1 {freq}\r\n".encode()
         time_command = f'STIM 1 {time} 1\r\n'.encode()
@@ -83,8 +113,7 @@ dispatcher.set_default_handler(surge)
 dispatcher.map("/EMS_CALIB/*", calib)
 dispatcher.set_default_handler(calib)
 
-com = serial.Serial(port='/dev/cu.SLAB_USBtoUART', baudrate=115200)
-
+com = serial.Serial(port=COM_PORT, baudrate=115200) #init communication with EMS
 
 if __name__ == '__main__':
     server = osc_server.OSCUDPServer((IP, PORT), dispatcher)
